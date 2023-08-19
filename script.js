3f1e1335b93fae85324774e8b1189dd950e4616d
sai shravan <saishravan384@gmail.com>
2023-08-19 14:58:29 +0530

    feat: Display theme color in the document

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const buttonEl = document.querySelector('button')
 const rootEl = document.documentElement
+const themeTextEl = document.querySelector('code')
+
+themeTextEl.innerText = 'light'
 
 const handleThemeChange = (e) => {
   rootEl.classList.toggle('dark')
+  themeTextEl.innerText = rootEl.classList.contains('dark') ? 'dark' : 'light'
 }
 
 buttonEl.addEventListener('click', handleThemeChange)
