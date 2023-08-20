810d30c1a8c131226748424deb16fac161fe53da
sai shravan <saishravan384@gmail.com>
2023-08-20 15:05:11 +0530

    feat: Update theme text on screen correctly

--- script.js
+++ script.js
@@ -2,11 +2,28 @@ const buttonEl = document.querySelector('button')
 const rootEl = document.documentElement
 const themeTextEl = document.querySelector('code')
 
-themeTextEl.innerText = 'light'
+let isThemeInverse = false
+if (
+  window.matchMedia &&
+  window.matchMedia('(prefers-color-scheme: dark)').matches
+) {
+  themeTextEl.innerText = 'dark'
+} else {
+  themeTextEl.innerText = 'light'
+}
 
 const handleThemeChange = (e) => {
   rootEl.classList.toggle('changed')
-  themeTextEl.innerText = rootEl.classList.contains('dark') ? 'dark' : 'light'
+  isThemeInverse = !isThemeInverse
+  updateThemeText()
+}
+
+const updateThemeText = () => {
+  if (themeTextEl.innerText === 'dark') {
+    themeTextEl.innerText = 'light'
+  } else if (themeTextEl.innerText === 'light') {
+    themeTextEl.innerText = 'dark'
+  }
 }
 
 buttonEl.addEventListener('click', handleThemeChange)
